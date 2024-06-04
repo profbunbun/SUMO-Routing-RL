0c49294611ac69150a6f84688116e4ea7a5e6e2e
profbunbun <aaronrls1984@gmail.com>
2024-06-03 21:22:05 -0400

    cleaned main

--- src/main.py
+++ src/main.py
@@ -1,55 +1,65 @@
-
+import argparse
 from utils import Utils
 from environment import sim_manager as so
 import wandb
 
-config = Utils.load_yaml_config('src/config/config.yaml')
-EPISODES = config['training_settings']['episodes']
+def parse_args():
+    """
+    Parse command-line arguments for hyperparameters and configurations.
+    """
+    parser = argparse.ArgumentParser(description="Reinforcement Learning for SUMO Traffic Simulation")
+    parser.add_argument('--config', type=str, default='src/config/config.yaml', help='Path to the configuration file')
+    parser.add_argument('--episodes', type=int, default=None, help='Number of episodes')
+    parser.add_argument('--learning_rate', type=float, default=None, help='Learning rate for the agent')
+    parser.add_argument('--batch_size', type=int, default=None, help='Batch size for training')
+    parser.add_argument('--epsilon_decay', type=float, default=None, help='Epsilon decay rate')
+    return parser.parse_args()
 
+def load_and_override_config(args):
+    """
+    Load configuration from a YAML file and override with command-line arguments if provided.
+    """
+    config = Utils.load_yaml_config(args.config)
+    if args.episodes is not None:
+        config['training_settings']['episodes'] = args.episodes
+    if args.learning_rate is not None:
+        config['agent_hyperparameters']['learning_rate'] = args.learning_rate
+    if args.batch_size is not None:
+        config['agent_hyperparameters']['batch_size'] = args.batch_size
+    if args.epsilon_decay is not None:
+        config['agent_hyperparameters']['epsilon_decay'] = args.epsilon_decay
+    return config
 
-wandb.init(project=config['wandb']['project_name'],
-           entity=config['wandb']['entity'],
-           name=config['wandb']['name'],
-           group=config['wandb']['group'],
-           config=config)
-
-def main_training_loop():
+def main_training_loop(config):
+    """
+    Main training loop for the reinforcement learning agent.
+    """
     env = so.create_env(config=config)
     dagent = so.create_agent(config=config)
-
     best_reward = float('-inf')
 
-
-
-
-    for episode in range(EPISODES):
+    for episode in range(config['training_settings']['episodes']):
         route_taken = []
         cumulative_reward = 0
         env.render()
         # env.render("gui")
         state = env.reset()
-
         done = 0
         
-        while done != 1:
-
-            action= dagent.choose_action(state)
-            next_state, new_reward, done,info = env.step(action)
+        while not done:
+            action = dagent.choose_action(state)
+            next_state, new_reward, done, info = env.step(action)
             edge = info
             route_taken.append(edge)
-            dagent.remember(state, action, new_reward, next_state, done )
+            dagent.remember(state, action, new_reward, next_state, done)
             cumulative_reward += new_reward
 
-
-
             if len(dagent.memory) > dagent.batch_size:
-
                 dagent.replay(dagent.batch_size)
                 dagent.hard_update()
+
             state = next_state
 
-
-        
         wandb.log({
             "cumulative_reward": cumulative_reward,
             "epsilon": dagent.get_epsilon(),
@@ -59,15 +69,24 @@ def main_training_loop():
             "Distance": env.distance_traveled
         })
 
-
-
         dagent.decay()
         env.close(episode, cumulative_reward, dagent.get_epsilon())
         if cumulative_reward > best_reward:
             best_reward = cumulative_reward
             dagent.save_model(episode)
-    
+
     wandb.finish()
 
 if __name__ == "__main__":
-    main_training_loop()
+    args = parse_args()
+    try:
+        config = load_and_override_config(args)
+        wandb.init(project=config['wandb']['project_name'],
+                   entity=config['wandb']['entity'],
+                   name=config['wandb']['name'],
+                   group=config['wandb']['group'],
+                   config=config)
+        main_training_loop(config)
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        wandb.finish(exit_code=1)
